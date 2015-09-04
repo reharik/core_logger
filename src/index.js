c10c4ac6e1e0318752bdebffdb52710e80d66b85
Raif Harik <reharik@gmail.com>
2015-09-03 23:06:15 -0500

    dagoning it

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@
  */
 
 var _logger = require('./modules/logger');
+var moment = require('moment');
 
-var logger = new _logger({
+module.exports = function(_options) {
+    var options = _options || {};
+    var logger = new _logger({
         system: {
             applicationName: process.env.APPLICATION_NAME || 'root',
-            environment: process.env.ENVIROMENT || 'dev'
+            environment: process.env.ENVIROMENT || 'development'
         }
     });
-    logger.addDailyRotateFileSink({
-            level:  'info',
+    logger.addConsoleSink({
+        level: options.consoleSink && options.consoleSink.level || options.level || 'silly',
+        colorize: true,
+        formatter: options.formatter
+        || function (x) {
+            return '[' + x.meta.level + '] module: '+options.moduleName+' msg: ' + x.meta.message + ' | ' + moment().format('h:mm:ss a');
+        }
+    }).info("added Console Sink")
+        .addDailyRotateFileSink({
+            level: options.fileSink && options.fileSink.level || options.level || 'info',
             filename: "/" + process.env.APPLICATION_NAME + ".log"
         })
         .info("added Daily RotateFile Sink");
-
-module.exports = logger;
+    return logger;
+};
