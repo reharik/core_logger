66064f026287e656351e96e51842245a6d78e6ec
Raif Harik <reharik@gmail.com>
2017-07-12 09:22:42 -0500

    adding some error handling

--- src/logger.js
+++ src/logger.js
@@ -1,134 +1,174 @@
-var winston = require('winston');
-var moment = require('moment');
-var os = require('os');
+const winston = require('winston');
+const moment = require('moment');
+const os = require('os');
 require('winston-logstash');
 require('winston-loggly-bulk');
 
 module.exports = function () {
-    var useTransports = process.env.LOGGING_TRANSPORTS;
+  const useTransports = process.env.LOGGING_TRANSPORTS;
 
-    var transports = [];
-    if(useTransports && useTransports.indexOf('logstash') >=0 )
-    {
-        var logstash = new (winston.transports.Logstash)({
-            port: 5000,
-            node_name: os.hostname(),
-            host: "mf_logstash",
-            // max_connect_retries: 10,
-            timeout_connect_retries: 1500
-        });
-        logstash.on('error', function(err) {
-            // console.error(err); // replace with your own functionality here
-        });
-        transports.push(logstash);
-    }
+  let transports = [];
+  if (useTransports && useTransports.indexOf('logstash') >= 0) {
+    let logstash = new (winston.transports.Logstash)({
+      port: 5000,
+      node_name: os.hostname(),
+      host: "mf_logstash",
+      // max_connect_retries: 10,
+      timeout_connect_retries: 1500
+    });
+    logstash.on('error', function (err) {
+      // console.error(err); // replace with your own functionality here
+    });
+    transports.push(logstash);
+  }
 
-  if(useTransports && useTransports.indexOf('loggly') >=0 )
-  {
-    var loggly = new (winston.transports.Loggly)({
+  if (useTransports && useTransports.indexOf('loggly') >= 0 && !!process.env.LOGGLY_TOKEN) {
+    let loggly = new (winston.transports.Loggly)({
       token: process.env.LOGGLY_TOKEN,
       subdomain: "methodfitness",
       tags: ["Winston-NodeJS"],
-      json:true,
+      json: true,
       level: process.env.LOGGING_LEVEL || 'info,',
       isBulk: true
     });
     transports.push(loggly);
   }
 
-    if(!useTransports || useTransports.indexOf('console') >= 0)
-        transports.push(
-          new (winston.transports.Console)({
-              handleExceptions: true,
-              prettyPrint: true,
-              colorize: true,
-              silent: false,
-              timestamp: true,
-              json: false,
-              formatter: (x) => {
-                  return `[${x.meta.level || x.level}] module: ${process.env.APPLICATION_NAME} msg: ${x.meta.details || x.details} | ${moment().format('h:mm:ss a')}`;
-              }
-          }));
-    
+  if (!useTransports || useTransports.indexOf('console') >= 0)
+    transports.push(
+      new (winston.transports.Console)({
+        handleExceptions: true,
+        prettyPrint: true,
+        colorize: true,
+        silent: false,
+        timestamp: true,
+        json: false,
+        formatter: (x) => {
+          return `[${x.meta.level || x.level}] module: ${process.env.APPLICATION_NAME} msg: ${x.meta.details || x.details} | ${moment().format('h:mm:ss a')}`;
+        }
+      }));
+
+  try {
     winston.configure({
-        transports,
-        level: process.env.LOGGING_LEVEL || 'silly'
+      transports,
+      level: process.env.LOGGING_LEVEL || 'silly'
+    });
+  } catch (err) {
+    console.log('==========error thrown in logger========');
+    console.log(err);
+    console.log('==========END error thrown in logger========');
+    throw err;
+  }
+
+  let message = {
+    system: {
+      environment: process.env.ENV,
+      applicationName: process.env.APPLICATION_NAME,
+      host: os.hostname(),
+      pid: process.pid
+    },
+    sprops: {},
+    nprops: {},
+    tags: [],
+    details: "",
+    type: "coreLogger"
+  };
+
+  function isNumeric(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+  }
+
+  function mapError(err, message) {
+    message.details = err.message;
+    message.stackTrace = err.stack;
+    err.keys && err.keys.forEach(key => {
+      if (isNumeric(err[key]))
+        message.nprops[key] = err[key];
+      else if (typeof (err[key]) === 'string')
+        message.sprops[key] = err[key];
     });
 
-    var message = {
-        system: {
-            environment: process.env.ENV,
-            applicationName: process.env.APPLICATION_NAME,
-            host: os.hostname(),
-            pid: process.pid
-        },
-        sprops: {},
-        nprops: {},
-        tags: [],
-        details: "",
-        type: "coreLogger"
-    };
+    return message;
+  }
 
-    function isNumeric(value) {
-        return !isNaN(parseFloat(value)) && isFinite(value);
+  function mapMessage(input, level) {
+    let newMessage = Object.assign({}, message);
+    if (input === null) {
+      return;
     }
+    newMessage.level = level;
+    message["@timestamp"] = moment().toISOString();
 
-    function mapError(err, message) {
-        message.details = err.message;
-        message.stackTrace = err.stack;
-        err.keys && err.keys.forEach(key => {
-            if (isNumeric(err[key]))
-                message.nprops[key] = err[key];
-            else if (typeof (err[key]) === 'string')
-                message.sprops[key] = err[key];
-        });
-
-        return message;
+    if (input instanceof Error) {
+      return mapError(input, newMessage);
     }
-
-    function mapMessage(input, level) {
-        var newMessage = Object.assign({}, message);
-        if (input == null) {
-            return;
-        }
-        newMessage.level = level;
-        message["@timestamp"] = moment().toISOString();
-
-        if (input instanceof Error) {
-            return mapError(input, newMessage);
-        }
-        if (input.error instanceof Error) {
-            return mapError(input.error, newMessage);
-        }
-        newMessage.details = input;
-        return newMessage;
+    if (input.error instanceof Error) {
+      return mapError(input.error, newMessage);
     }
+    newMessage.details = input;
+    return newMessage;
+  }
 
-    var trace = (message) => {
-        winston.silly(mapMessage(message, 'trace'));
-    };
-
-    var debug = (message) => {
-        winston.debug(mapMessage(message, 'debug'));
-    };
-
-    var info = (message) => {
-        winston.info(mapMessage(message, 'info'));
-    };
-
-    var warn = (message) => {
-        winston.warn(mapMessage(message, 'warn'));
-    };
-
-    var error = (message) => {
-        winston.error(mapMessage(message, 'error'));
-    };
-
-    return {
-        trace,
-        debug,
-        info,
-        warn,
-        error
+  const trace = (message) => {
+    try {
+      winston.silly(mapMessage(message, 'trace'));
+    } catch (err) {
+      console.log('==========error thrown in logger========');
+      console.log(err);
+      console.log('==========END error thrown in logger========');
+      throw err;
     }
+  };
+
+  const debug = (message) => {
+    try {
+      winston.debug(mapMessage(message, 'debug'));
+    } catch (err) {
+      console.log('==========error thrown in logger========');
+      console.log(err);
+      console.log('==========END error thrown in logger========');
+      throw err;
+    }
+  };
+
+  const info = (message) => {
+    try {
+      winston.info(mapMessage(message, 'info'));
+    } catch (err) {
+      console.log('==========error thrown in logger========');
+      console.log(err);
+      console.log('==========END error thrown in logger========');
+      throw err;
+    }
+  };
+
+  const warn = (message) => {
+    try {
+      winston.warn(mapMessage(message, 'warn'));
+    } catch (err) {
+      console.log('==========error thrown in logger========');
+      console.log(err);
+      console.log('==========END error thrown in logger========');
+      throw err;
+    }
+  };
+
+  const error = (message) => {
+    try {
+      winston.error(mapMessage(message, 'error'));
+    } catch (err) {
+      console.log('==========error thrown in logger========');
+      console.log(err);
+      console.log('==========END error thrown in logger========');
+      throw err;
+    }
+  };
+
+  return {
+    trace,
+    debug,
+    info,
+    warn,
+    error
+  }
 };
